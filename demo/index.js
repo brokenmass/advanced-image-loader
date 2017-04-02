2833f9122cde16309f3c1ad3af6096a70a4141e7
Marco Massarotto <marco.massarotto@mendeley.com>
2017-04-02 20:06:28 +0100

    docs: add project core documentation

---
+++ demo/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-unresolved, no-console */
+import image from './assets/test.png?width=400&quality=90&placeholder=32&srcset[]=200&srcset[]=400&srcset[]=800';
+
+console.log(JSON.stringify(image, null, 2));
